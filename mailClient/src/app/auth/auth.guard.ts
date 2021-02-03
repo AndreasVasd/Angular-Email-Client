0229ac1a8a462b006e48ac24c79b2018a5b6997d
Andreas Vasd <andreas_1996@windowslive.com>
2021-02-03 15:22:16 +0200

    Finish Authentication

--- mailClient/src/app/auth/auth.guard.ts
+++ mailClient/src/app/auth/auth.guard.ts
@@ -1,22 +1,28 @@
 import { Injectable } from '@angular/core';
-import { CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Router, CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
-import { take, skipWhile } from 'rxjs/operators';
+import { take, skipWhile, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanLoad {
 
-constructor(private authService: AuthService) {}
+constructor(private authService: AuthService,
+            private router: Router) {}
 
   canLoad(
     route: Route,
     segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return this.authService.signedin$.pipe(
       skipWhile(value => value === null),
-      take(1)
+      take(1),
+      tap((authenticated) => {
+        if(!authenticated) {
+          this.router.navigateByUrl('/'); //αν δεν ειναι authenticated κ επιχειρησει να παει στο /inbox απ το url
+        }
+      })
     );
   }
 }
