07bc0a5371608e5cd5d6484d453e8db0f2bb019c
Andreas Vasd <andreas_1996@windowslive.com>
2021-02-06 19:35:24 +0200

    EmailShow with switchMap

--- mailClient/src/app/inbox/email-show/email-show.component.ts
+++ mailClient/src/app/inbox/email-show/email-show.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router'; //for getting access to url
 import { EmailService } from '../email.service';
-
+import { switchMap }from 'rxjs/operators';
+import { Email } from '../email';
+ 
 @Component({
   selector: 'app-email-show',
   templateUrl: './email-show.component.html',
@@ -9,17 +11,34 @@ import { EmailService } from '../email.service';
 })
 export class EmailShowComponent implements OnInit {
 
+  email: Email;
+
+
   constructor(private route: ActivatedRoute, private emailService: EmailService) { }
 
   ngOnInit() {
     console.log(this.route);
+    /* 1ST WAY
     //params is a Behavior Subject that emits events every time the url changes..so we subscribe to url changes 
     //(that happen when user clicks an email)
-    this.route.params.subscribe(({ id }) => {
+    this.route.params.subscribe(({ id }) => { //we pull out id property via destructuring form params object
       this.emailService.getEmail(id).subscribe(email => { //παίρνουμε το συγκεκριμενο mail βασει του id του
         console.log(email);
       })
-    })
+    })*/
+
+    //2ND WAY - with switchMap
+    this.route.params
+      .pipe(
+        switchMap(( { id }) => {
+          return this.emailService.getEmail(id);
+        })
+      )
+      .subscribe(email => {
+        //console.log(email);
+        this.email = email;
+      });
+
   }
 
 }
