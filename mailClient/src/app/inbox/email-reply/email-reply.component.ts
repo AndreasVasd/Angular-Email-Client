0c6beedf22e77da2d07043e5203f022e707441a6
Andreas Vasd <andreas_1996@windowslive.com>
2021-02-09 16:49:16 +0200

    The end

--- mailClient/src/app/inbox/email-reply/email-reply.component.ts
+++ mailClient/src/app/inbox/email-reply/email-reply.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Email } from '../email';
+import { EmailService } from '../email.service';
 
 @Component({
   selector: 'app-email-reply',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./email-reply.component.css']
 })
 export class EmailReplyComponent implements OnInit {
+  showModal = false;
+  @Input() email: Email; //αντιστοιχει στο [email] που βρισκεται εντος του email.show.html
 
-  constructor() { }
+  constructor(private emailService: EmailService) { }
 
-  ngOnInit(): void {
+  ngOnChanges() {  //αν δε το αντιστρεψω όταν κανω reply το from εμφανιζεται στη θεση του to κι αναποδα
+    this.email = {
+      ...this.email,
+      from: this.email.to,
+      to: this.email.from,
+      text: `\n\n\n ${this.email.from} wrote:\n ${this.email.text}`
+    };
+  }
+
+  onSubmit(email: Email) {
+    this.emailService.sendEmail(email).subscribe(() => {
+      this.showModal = false;
+    });
   }
 
 }
